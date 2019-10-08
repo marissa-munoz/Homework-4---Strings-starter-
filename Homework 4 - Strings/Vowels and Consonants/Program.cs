7a70775a37054b9b444d8e159287fea85e75bac5
Marissa Munoz <marissamunoz@ou.edu>
2019-10-08 11:50:46 -0500

    starter code

---
+++ Homework 4 - Strings/Vowels and Consonants/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/***
+ * Prompt the user to input a sentence.  
+ * Once you have their sentence, figure out how many vowels (a, e, i, o, u) and how many consonants are in the sentence.  
+ * Output the total number of consonants and vowels in the following format (2 separate lines and the format specifier should be general with 0 decimal places!)
+There were 5 vowels in the sentence.
+There were 15 consonants in the sentence.
+ * 
+ ***/
+namespace Vowels_and_Consonants
+{
+    class Program
+    {
+        //name here
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Homework 4 - Strings/l337 Converter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/***
+ * Prompt the user to input a sentence.  
+ * Once you have their sentence (Convert the sentence to all uppercase), you will convert their sentence to 'l337' (Links to an external site.).  
+ * You will need to replace the letter on the left with the letter on the right, using the replace() function in the String class, from the following table:
+
+A	@
+E	3
+H	|-|
+S	$
+T	7
+U	|_|
+Since we are very excited about our application, we want to make sure that every sentence ends with an exclamation mark!  
+If it already ends with an exclamation mark, do not add another, otherwise concatenate a '!' at the end!
+ * 
+ * 
+ ***/
+namespace l337_Converter
+{
+    class Program
+    {
+        //name here
+        static void Main(string[] args)
+        {
+        }
+    }
+}
